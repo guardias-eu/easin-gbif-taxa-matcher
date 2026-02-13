af38eea38c48dd7a156344ed00171fbb5745ce5a
Damiano Oldoni <damiano.oldoni@inbo.be>
2026-02-13 11:05:28 +0100

    Correct organisation name to find reasin

--- src/install_packages.R
+++ src/install_packages.R
@@ -10,7 +10,7 @@ required <- c(
 if (!all(required %in% installed)) {
   # reasin is not on CRAN, so we need to install it from GitHub
   if (!"reasin" %in% installed) {
-    remotes::install_github("inbo/reasin")
+    remotes::install_github("guardias-eu/reasin")
   }
   required <- required[required != "reasin"]
   pkgs_to_install <- required[!required %in% installed]
